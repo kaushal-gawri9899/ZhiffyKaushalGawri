912f65ad82d6c2f34fc062c42a7da2ae2c7dfefa
kaushal-gawri9899 <gawrikaushal9899@gmail.com>
2021-07-14 17:13:53 +0530

    Implemented all CRUD operations for various end points and tested on Postman

--- Zhiffy/application.py
+++ Zhiffy/application.py
@@ -20,11 +20,13 @@ dbClient = MongoClient("mongodb://localhost:27017/")
 db = dbClient["application_db"]
 
 zhiffy = db["Zhiffy"]
+items = db["Items"]
 
 app = Flask(__name__)
 jwt = JWTManager(app)
 
 app.config["JWT_SECRET_KEY"] = "ACCESS_KEY_999"
+#mongo = PyMongo(app)
 
 
 @app.route("/register", methods=["POST"])
@@ -45,23 +47,10 @@ def register():
 
         return jsonify(message="Voila! User Registration Successful.", access_token=user_access_token), 201
 
+
 @app.route("/login", methods=["POST"])
 def login():
-    # _json = request.json
 
-    # if request.is_json:
-    #     user_email = _json["email"]
-    #     user_password = _json["password"]
-    #     hash_password = generate_password_hash(user_password)
-    
-    # print(hash_password)
-    # is_present = zhiffy.find_one({"email": user_email, "password": hash_password})
-
-    # if is_present:
-    #     user_access_token = create_access_token(identity=user_email)
-    #     return jsonify(message="Voila! User Successfully Logged In.", access_token=user_access_token)
-    # else:
-    #     return jsonify(message="Wrong Credentials. Please Retry."), 401
     _json = request.json
     user_email = _json["email"]
     user_password = _json["password"]
@@ -75,6 +64,83 @@ def login():
 
     return "Invalid Credentials. Please Retry."
 
+@app.route("/changeUserDetails/<uid>", methods=["PUT"])
+@jwt_required()
+def changeUserDetail(uid):
+    _json = request.json
+    user_email = _json["email"]
+    user_name = _json["name"]
+    user_password = _json["password"]
+    hash_password = bcrypt.hashpw(user_password.encode('utf-8'), bcrypt.gensalt())
+
+    update_user = zhiffy.update_one({'_id': ObjectId(uid)}, {"$set": {'email': user_email, 'name': user_name, 'password': hash_password}})
+
+    return jsonify(message="User Details with Email {"+user_email+"} and ID {"+str(ObjectId(uid))+"} Updated.")
+
+
+@app.route("/insertProducts", methods=["POST"])
+@jwt_required()
+def insertProducts():
+    _json = request.json
+    item_category = _json["category"]
+    item_brand = _json["brand"]
+    item_model = _json["model"]
+    item_purchase = _json["purchase_date"]
+    item_price = _json["price"]
+    item_location = _json["location"]
+
+    if item_category and item_brand and item_purchase and item_purchase and item_price and item_location and request.method == 'POST':
+        item_data = dict(category=item_category, brand=item_brand, model=item_model, purchase_date=item_purchase, price=item_price, location=item_location)
+        
+        id = items.insert_one(item_data)
+        return jsonify(message="Item Added Successfully")
+    
+    return jsonify(message="Please complete all the fields")
+
+
+
+@app.route("/seeAllProducts", methods=["GET"])
+def getAllItems():
+    allItems = items.find()
+    results = dumps(allItems)
+    return results
+
+
+
+@app.route("/seeAllProducts/<pid>", methods=["GET"])
+def getItemDetails(pid):
+    item = items.find_one({"_id": ObjectId(pid)})
+    result = dumps(item)
+    return result
+
+
+
+@app.route("/deleteProduct/<pid>", methods=["DELETE"])
+def deleteGivenItem(pid):
+    
+    result = jsonify("Item with ID {"+str(ObjectId(pid))+"} Deleted Successfully.")
+    items.delete_one({"_id": ObjectId(pid)})
+    result.status_code = 200
+
+    return result
+
+    
+@app.route("/changeItemDetails/<pid>", methods=["PUT"])
+def changeItemDetail(pid):
+    _json = request.json
+
+    item_category = _json["category"]
+    item_brand = _json["brand"]
+    item_model = _json["model"]
+    item_purchase = _json["purchase_date"]
+    item_price = _json["price"]
+    item_location = _json["location"]
+
+    update_item = items.update_one({'_id': ObjectId(pid)}, {"$set": {'category': item_category, 'brand': item_brand, 'model': item_model, 'purchase_date': item_purchase, 'price': item_price, 'location': item_location}})
+    
+    return jsonify(message="Item Details with ID {"+str(ObjectId(pid))+"} Updated.")
+
+    
 
 if __name__ == '__main__':
     app.run(host="localhost", debug=True)
